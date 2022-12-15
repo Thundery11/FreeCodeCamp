4f681a3b977cd58e9cad59e7c9bbb767c4414aa4
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2022-12-15 18:11:34 +0300

    Update 90-100.js

---
+++ 90-100.js
@@ -0,0 +1,17 @@
+//90
+// Setup
+const myArray = [];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+// Only change code below this line
+//91
+// Setup
+const myArray2 = [];
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+// Only change code below this line
+//92
