e8b8397e2464b8e965bb03a902a2a66e36f3a911
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2022-12-17 18:16:05 +0300

    Update 90-100.js

--- 90-100.js
+++ 90-100.js
@@ -55,3 +55,14 @@ multiplyAll([
   [3, 4],
   [5, 6, 7],
 ]);
+
+//96 
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+}while (i < 11)
