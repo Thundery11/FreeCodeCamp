24b051888338e499d1ef99d059d64f0ba50572af
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2022-12-22 17:45:17 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -29,3 +29,19 @@ function checkSign(num) {
 
 checkSign(10);
 //105
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
