8604cfb32155134796679b2acec6df3f4015bb5a
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-02 22:15:00 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -161,4 +161,5 @@ const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
-xcxcxcxcxcxcx;
+//116
+dlfldlfdl;
