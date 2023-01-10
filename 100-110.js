d569ef9d8b4494befa3f128ae9b26e10026b647b
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-10 18:18:50 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -175,3 +175,9 @@ import { uppercaseString, lowercaseString } from "./string_functions.js";
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+//118
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
