41fe6d3724d8f8b8ee84f254e3b4a3dcbe40fe6d
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-11 20:40:30 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -210,3 +210,7 @@ let result = waldoRegex.test(waldoIsHiding);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+//125
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
