641451cdbde5175937fdef915f805a9f29aae3c2
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-12 18:45:59 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -218,3 +218,4 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
+//127
