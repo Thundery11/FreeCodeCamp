6d6bc20d984f07c30041c24065ba0fdfc1d44070
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-13 17:07:12 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -218,4 +218,3 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; // Change this line
 let result = extractStr.match(codingRegex); // Change this line
-//127
