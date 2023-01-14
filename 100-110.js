0225024481c5510dbc169a0f182c186eb222f7ff
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-14 19:18:32 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -231,3 +231,7 @@ let quoteSample =
   "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
+//130
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
