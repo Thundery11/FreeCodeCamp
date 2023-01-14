fe9503b5318cd907e565e13c412257e56a4c7c43
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-01-14 19:20:16 +0300

    Update 100-110.js

--- 100-110.js
+++ 100-110.js
@@ -235,3 +235,7 @@ let result = quoteSample.match(vowelRegex); // Change this line
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/gi; // Change this line
 let result = quoteSample.match(alphabetRegex); // Change this line
+//131
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
