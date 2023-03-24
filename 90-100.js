4f2fe1209484ed72d54a025d344c96271cdf38ea
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-03-24 15:49:43 +0300

    .

--- 90-100.js
+++ 90-100.js
@@ -1,135 +1,135 @@
-//90
-// Setup
-const myArray = [];
-let i = 5;
-while (i >= 0) {
-  myArray.push(i);
-  i--;
-}
-// Only change code below this line
-//91
-// Setup
-const myArray2 = [];
-for (let i = 1; i <= 5; i++) {
-  myArray.push(i);
-}
-// Only change code below this line
-//92
-// Setup
-const myArray3 = [];
-for (let i = 1; i < 10; i += 2) {
-  myArray.push(i);
-}
-// Only change code below this line
-//93
-// Setup
-const myArray4 = [];
-for (let i = 9; i > 0; i -= 2) {
-  myArray.push(i);
-}
-//94
-// Only change code below this line
-// Setup
-const myArr = [2, 3, 4, 5, 6];
-let total = 0;
-for (let i = 0; i < myArr.length; i++) {
-  total += myArr[i];
-}
-// Only change code below this line
+// //90
+// // Setup
+// const myArray = [];
+// let i = 5;
+// while (i >= 0) {
+//   myArray.push(i);
+//   i--;
+// }
+// // Only change code below this line
+// //91
+// // Setup
+// const myArray2 = [];
+// for (let i = 1; i <= 5; i++) {
+//   myArray.push(i);
+// }
+// // Only change code below this line
+// //92
+// // Setup
+// const myArray3 = [];
+// for (let i = 1; i < 10; i += 2) {
+//   myArray.push(i);
+// }
+// // Only change code below this line
+// //93
+// // Setup
+// const myArray4 = [];
+// for (let i = 9; i > 0; i -= 2) {
+//   myArray.push(i);
+// }
+// //94
+// // Only change code below this line
+// // Setup
+// const myArr = [2, 3, 4, 5, 6];
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//   total += myArr[i];
+// }
+// // Only change code below this line
 
-//95
-function multiplyAll(arr) {
-  let product = 1;
-  // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      product *= arr[i][j];
-    }
-  }
-  // Only change code above this line
-  return product;
-}
+// //95
+// function multiplyAll(arr) {
+//   let product = 1;
+//   // Only change code below this line
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       product *= arr[i][j];
+//     }
+//   }
+//   // Only change code above this line
+//   return product;
+// }
 
-multiplyAll([
-  [1, 2],
-  [3, 4],
-  [5, 6, 7],
-]);
+// multiplyAll([
+//   [1, 2],
+//   [3, 4],
+//   [5, 6, 7],
+// ]);
 
-//96
-// Setup
-const myArray = [];
-let i = 10;
+// //96
+// // Setup
+// const myArray = [];
+// let i = 10;
 
-// Only change code below this line
-do {
-  myArray.push(i);
-  i++;
-} while (i < 11);
+// // Only change code below this line
+// do {
+//   myArray.push(i);
+//   i++;
+// } while (i < 11);
 
-//97
-function sum(arr, n) {
-  // Only change code below this line
-  if (n <= 0 && n >= -8) {
-    return 0;
-  } else {
-    return sum(arr, n - 1) + arr[n - 1];
-  }
-}
-// Only change code above this line
-//98
-let dhds = () => {
-  console.log("hello world");
-};
+// //97
+// function sum(arr, n) {
+//   // Only change code below this line
+//   if (n <= 0 && n >= -8) {
+//     return 0;
+//   } else {
+//     return sum(arr, n - 1) + arr[n - 1];
+//   }
+// }
+// // Only change code above this line
+// //98
+// let dhds = () => {
+//   console.log("hello world");
+// };
 
-dhds();
-//99
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+// dhds();
+// //99
+// // Setup
+// const contacts = [
+//   {
+//     firstName: "Akira",
+//     lastName: "Laine",
+//     number: "0543236543",
+//     likes: ["Pizza", "Coding", "Brownie Points"],
+//   },
+//   {
+//     firstName: "Harry",
+//     lastName: "Potter",
+//     number: "0994372684",
+//     likes: ["Hogwarts", "Magic", "Hagrid"],
+//   },
+//   {
+//     firstName: "Sherlock",
+//     lastName: "Holmes",
+//     number: "0487345643",
+//     likes: ["Intriguing Cases", "Violin"],
+//   },
+//   {
+//     firstName: "Kristian",
+//     lastName: "Vos",
+//     number: "unknown",
+//     likes: ["JavaScript", "Gaming", "Foxes"],
+//   },
+// ];
 
-function lookUpProfile(name, prop) {
-  // Only change code below this line
-  for (let i = 0; i < contacts.length; i++) {
-    if (contacts[i].firstName === name) {
-      if (contacts[i].hasOwnProperty(prop)) {
-        return contacts[i][prop];
-      } else {
-        return "No such property";
-      }
-    }
-  }
-  return "No such contact";
-}
-//100
-function randomFraction() {
-  // Only change code below this line
+// function lookUpProfile(name, prop) {
+//   // Only change code below this line
+//   for (let i = 0; i < contacts.length; i++) {
+//     if (contacts[i].firstName === name) {
+//       if (contacts[i].hasOwnProperty(prop)) {
+//         return contacts[i][prop];
+//       } else {
+//         return "No such property";
+//       }
+//     }
+//   }
+//   return "No such contact";
+// }
+// //100
+// function randomFraction() {
+//   // Only change code below this line
 
-  return Math.random();
+//   return Math.random();
 
-  // Only change code above this line
-}
+//   // Only change code above this line
+// }
